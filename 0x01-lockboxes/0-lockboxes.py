eb48d9ceb053a158d200fabbdffa22a342fa07d2
ruizdani301 <ruizdani301@gmail.com>
2022-08-04 11:26:05 -0500

    lockboxes

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+""" lockboxes challenge """
+
+
+def canUnlockAll(boxes):
+    """this function return True if each array has the key
+        that permit to get into the respective array.
+    """
+
+    array = []
+    for x in boxes[0]:
+        array.append(x)
+
+    for date in array:
+        for y in boxes[date]:
+            if y in array:
+                pass
+            else:
+                array.append(y)
+        i = 0
+        for vacio in boxes:
+
+            if not vacio:
+                i = i + 1
+        pass
+    if len(boxes) - i == len(array):
+        return True
+    return False
