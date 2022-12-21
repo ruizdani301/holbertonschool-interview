7799643632e67efff4b654fdf2eae6e780614f75
ruizdani301 <ruizdani301@gmail.com>
2022-12-21 12:53:04 -0500

    UDP: task_log_parsing

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,26 +1,44 @@
 #!/usr/bin/python3
-""" Log parsing"""
+"""Log parsing"""
+import signal
 import sys
 
 
-t_size = 0
-s_codes = {"200": 0, "301": 0, "400": 0, "401": 0,
-           "403": 0, "404": 0, "405": 0, "500": 0}
-try:
-    for count, log in enumerate(sys.stdin, 1):
-        splt = log.split(" ")
-        if len(splt) < 2:
-            continue
-        if splt[-2] in s_codes:
-            s_codes[splt[-2]] += 1
-        t_size += int(splt[-1])
-        if count % 10 == 0:
-            print("File size: {}".format(t_size))
-            for key, value in sorted(s_codes.items()):
-                if value > 0:
-                    print("{}: {}".format(key, value))
-finally:
-    print("File size: {}".format(t_size))
-    for key, value in sorted(s_codes.items()):
-        if value > 0:
-            print("{}: {}".format(key, value))
+if __name__ == '__main__':
+    sum_file_size = 0
+    status_code = {'200': 0,
+                   '301': 0,
+                   '400': 0,
+                   '401': 0,
+                   '403': 0,
+                   '404': 0,
+                   '405': 0,
+                   '500': 0}
+
+    try:
+        i = 0
+        for line in sys.stdin:
+            args = line.split()
+            if len(args) > 6:
+                status_line = args[-2]
+                file_size = args[-1]
+                sum_file_size += int(file_size)
+                if status_line in status_code:
+                    i += 1
+                    status_code[status_line] += 1
+                    if i % 10 == 0:
+                        print('File size: {}'.format(sum_file_size))
+                        for key, value in sorted(status_code.items()):
+                            if value != 0:
+                                print('{}: {}'.format(key, value))
+
+        print('File size: {}'.format(sum_file_size))
+        for key, value in sorted(status_code.items()):
+            if value != 0:
+                print('{}: {}'.format(key, value))
+
+    except KeyboardInterrupt:
+        print('File size: {}'.format(sum_file_size))
+        for key, value in sorted(status_code.items()):
+            if value != 0:
+                print('{}: {}'.format(key, value))
